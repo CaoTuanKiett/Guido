6ca00dff597912516df07a9956349d90cc9d0dea
kenzie.cao <kenzie.cao@avepoint.com>
2024-03-12 19:22:34 +0700

    fix router

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
-import AutoImport from "unplugin-auto-import";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Pages({
-      dirs: ["./src/pages"],
-    }),
-    AutoImport({
-      imports: ["vue", "vue-router"],
-      dts: "./src/vite-env.d.ts",
+      dirs: "./src/page",
     }),
   ],
 });
