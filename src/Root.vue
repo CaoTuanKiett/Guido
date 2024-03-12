c0948159a85a9622084438876aef5e704ab76795
kenzie.cao <kenzie.cao@avepoint.com>
2024-03-12 19:44:22 +0700

    fix router

--- src/Root.vue
+++ src/Root.vue
@@ -1,42 +1,20 @@
 <script setup lang="ts">
-// import { computed } from 'vue';
-// import { useRoute } from 'vue-router';
-
-// import DefaultLayout from './layouts/DefaultLayout.vue';
-// import AuthLayout from './layouts/AuthLayout.vue';
-
-// const route = useRoute();
-// const currentMeta = computed(()=> route.meta.layout || 'default');
-
-// const layoutMap = {
-//   default: DefaultLayout,
-//   auth: AuthLayout,
-// };
-
-// const VNodeLayout = computed(() => layoutMap[currentMeta.value]);
-
-import { defineComponent, computed } from "vue";
+import { computed } from "vue";
 import { useRoute } from "vue-router";
 
 import DefaultLayout from "./layouts/DefaultLayout.vue";
 import AuthLayout from "./layouts/AuthLayout.vue";
 
-export default defineComponent({
-  setup() {
-    const route = useRoute();
-    const currentMeta = computed(() => route.meta.layout || "default");
+const route = useRoute();
+const currentMeta = computed(() => route.meta.layout || "default");
 
-    const layoutMap: Record<string, any> = {
-      default: DefaultLayout,
-      auth: AuthLayout,
-    };
+const layoutMap = {
+  default: DefaultLayout,
+  auth: AuthLayout,
+};
 
-    const VNodeLayout = computed(() => layoutMap[currentMeta.value]);
-
-    return {
-      VNodeLayout,
-    };
-  },
+const VNodeLayout = computed(() => {
+  layoutMap[currentMeta.value];
 });
 </script>
 
